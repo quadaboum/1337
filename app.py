508f029fd50729d897004e7fb3c9fbae3912553f
quadaboum <quentin.bellenger@hotmail.fr>
2025-05-13 13:53:31 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def dashboard():
     return render_template('dashboard.html', user=session['username'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
