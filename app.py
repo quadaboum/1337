313b514f5d4bf97e7b298734eaf927688bec2a1e
quadaboum <quentin.bellenger@hotmail.fr>
2025-05-13 23:21:08 +0200

    Connexion PostgreSQL via PG* OK

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def get_db_connection():
         host=os.environ.get("PGHOST"),
         database=os.environ.get("PGDATABASE"),
         user=os.environ.get("PGUSER"),
-        password=os.environ.get("PGPASSWORD")
+        password=os.environ.get("PGPASSWORD"),
+        port=os.environ.get("PGPORT", 5432)
     )
 
 @app.route("/")
